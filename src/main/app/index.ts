2fc6ba62203bc9d326c12affd0afd9ca34d34741
Brendan Black <bblack@bluestep.net>
2025-08-20 14:51:04 -0600

    refactor command lifecycle management by removing unused disposables and updating context handling

--- src/main/app/index.ts
+++ src/main/app/index.ts
@@ -1,16 +1,18 @@
 import * as vscode from 'vscode';
 import { pullScript, pushScript } from './ctrl-p-commands';
-// The command has been defined in the package.json file
-// Now provide the implementation of the command with registerCommand
-// The commandId parameter must match the command field in package.json
-export const b6p_disposables: vscode.Disposable[] = [
-  vscode.commands.registerCommand('bsjs-push-pull.pullScript', pullScript),
-  vscode.commands.registerCommand('bsjs-push-pull.pushScript', pushScript),
-];
+
+
 export const State = new class {
   #context: vscode.ExtensionContext | null;
-  static vars: Map<string, any> = new Map<string, any>();
+  #vars: Map<string, any> = new Map<string, any>();
 
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  #disposables: vscode.Disposable[] = [
+    vscode.commands.registerCommand('bsjs-push-pull.pullScript', pullScript),
+    vscode.commands.registerCommand('bsjs-push-pull.pushScript', pushScript),
+  ];
   constructor() {
     this.#context = null;
   }
@@ -27,6 +29,7 @@ export const State = new class {
       throw new Error('Extension context is already set');
     }
     this.#context = context;
+    this.#disposables.forEach(disposable => context.subscriptions.push(disposable));
   }
 }();
 
--- src/main/lifecycle-control/end/index.ts
+++ src/main/lifecycle-control/end/index.ts
@@ -1,4 +1,3 @@
-import { b6p_disposables } from '../../app';
 export default function () {
-  b6p_disposables.forEach(disposable => disposable.dispose());
+  // TODO
 }
