5879d29f1ff37bd8758c51d39daa79d2becb87a1
Brendan Black <bblack@bluestep.net>
2025-08-26 15:33:34 -0600

    fix: update pullScript URL to remove 'draft/' path for correct file retrieval

--- src/main/app/ctrl-p-commands/scripts/pullCurrent.ts
+++ src/main/app/ctrl-p-commands/scripts/pullCurrent.ts
@@ -11,7 +11,7 @@ export default async function (): Promise<void> {
   console.log("Active Editor URI:", activeEditorUri.toString());
   console.log("workspace URI:", workspaceUri.toString());
   const { webdavId, domain } = await getFileMetaData({ workspaceUri: workspaceUri, curUri: activeEditorUri });
-  pullScript(`https://${domain}/files/${webdavId}/draft/`);
+  pullScript(`https://${domain}/files/${webdavId}/`);
 
 }
 
