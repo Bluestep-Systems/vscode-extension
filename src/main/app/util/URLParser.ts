990811a6ca2266501766e769efd1dad5d4db0ee0
Brendan Black <bblack@bluestep.net>
2025-08-26 19:00:28 -0600

    fix: include type in URLParser return object for better URL handling

--- src/main/app/util/URLParser.ts
+++ src/main/app/util/URLParser.ts
@@ -2,7 +2,8 @@ export function urlParser(str: string) {
   //const str = "https://bst3.bluestep.net/files/1433697/draft/";
   const url = new URL(str);
   const match = url.pathname.match(/\/(files|public)\/(\d+)\/(.*)/);
+  const type = match && match[1] || (() => { throw new Error("no type"); })(); // type is match[1], which is the first capture group (files|public)
   const webDavId = match && match[2] || (() => { throw new Error("no webDavId"); })(); // webDavId is match[2], which is the second capture group (\d+); for the example URL, this will be "1433697"
   const trailing = match && match[3];
-  return { url, webDavId, trailing };
+  return { url, type, webDavId, trailing } as { url: URL; type: "files" | "public"; webDavId: string; trailing: string | undefined };
 }
