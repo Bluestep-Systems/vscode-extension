bf8cc82bb2176a8bf56a510b0b404d7b81322778
Brendan Black <bblack@bluestep.net>
2025-08-29 15:37:24 -0600

    fix: update output for master tag in gittag.sh to clarify versioning

--- gittag.sh
+++ gittag.sh
@@ -6,7 +6,8 @@ if [ -n "${OVERRIDE_GIT_TAG}" ]; then
 fi
 TAG=$(git describe --tags --exact-match 2> /dev/null || git symbolic-ref -q --short HEAD || git rev-parse --short HEAD)
 if [[ "master" = "$TAG" ]]; then
-  echo latest
+  echo master #the original version of this script uses "latest" since it ties in with the github tagging/CI
+  # We'll keep this here as a placeholder for now until it is resolved properly
 else
   echo $TAG
 fi
