857e30cd4609dd49e26cecf81add43aa4b3a9e50
Brendan Black <bblack@bluestep.net>
2025-10-15 10:40:44 -0600

    Release v1.1.17

--- src/main/app/ctrl-p-commands/testTask.ts
+++ src/main/app/ctrl-p-commands/testTask.ts
@@ -1,9 +1,10 @@
 import { DownstairsUriParser } from "../util/data/DownstairsUrIParser";
 import * as vscode from 'vscode';
-
+import * as path from 'path';
 export default async function () {
   const TEST_URI = "/home/brendan/test/extensiontest/U131364/Typedoc Check/draft/static/index.html";
   const parser = new DownstairsUriParser(vscode.Uri.parse(TEST_URI));
+  path.relative("/home/brendan/test/extensiontest/U131364/Typedoc Check/draft/static/index.html","/home/brendan/test/extensiontest/U131364/Typedoc Check/draft/static/test2/thing");
   console.log(parser);
   debugger;
   
