1053be7c19006045ca36af58d0b79a594be682a6
Brendan Black <bblack@bluestep.net>
2026-03-20 13:58:34 -0600

    fix reference to setup jsp pages

--- src/main/app/ctrl-p-commands/goToSetup.ts
+++ src/main/app/ctrl-p-commands/goToSetup.ts
@@ -9,7 +9,16 @@ export default async function (): Promise<void> {
     const scriptRoot = ScriptFactory.createFile(contextualUri).getScriptRoot();
     const scriptKey = await scriptRoot.getScriptKey();
     const origin = await scriptRoot.anyOrigin();
-    const setupUrl = `${origin}shared/admin/applications/relate/editendpoint.jsp?_event=edit&_id=${scriptKey.classid}___${scriptKey.seqnum}`;
+    const keyToScript = {
+      "654015": "editformuladetails.jsp",
+      "530024": "editdetailreport1.jsp",
+      "363769": "editendpoint.jsp"
+    } as {[key: string]: string}; //TODO improve typing on this and scriptkey classid return type
+    const jspPage = keyToScript[scriptKey.classid];
+    if (!jspPage) {
+      throw new Error("no jsp page matching given id");
+    }
+    const setupUrl = `${origin}shared/admin/applications/relate/${jspPage}?_event=edit&_id=${scriptKey.classid}___${scriptKey.seqnum}`;
     await vscode.env.openExternal(vscode.Uri.parse(setupUrl));
   } catch (e) {
     Alert.error("Error opening setup: " + (e instanceof Error ? e.message : e));
